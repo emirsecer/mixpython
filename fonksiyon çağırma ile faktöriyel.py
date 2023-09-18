c0cdca86ecadf40cce131d0a8d57ca7402eb33c1
emirsecer <135528914+emirsecer@users.noreply.github.com>
2023-09-18 21:23:53 +0300

    Python öğrenirken yaptığım bir çalışma

---
+++ fonksiyon çağırma ile faktöriyel.py
@@ -0,0 +1,14 @@
+def factoriel(numara):
+    faktoriyel =1
+    for i in range (1,numara+1):
+        faktoriyel *=i
+    print("faktöryel=> ",faktoriyel)
+
+
+
+
+
+
+sayi=int(input("sayı girin"))
+
+factoriel(sayi)
