95d564b4d58acf690bdf0908dd0baae39818aff6
emirsecer <135528914+emirsecer@users.noreply.github.com>
2023-09-18 21:29:33 +0300

    Python öğrenirken yaptığım bir çalışma

---
+++ sqlite.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+con=sqlite3.connect("dersler.db")
+
+cursor=con.cursor()
+
+def tabloolustur():
+  cursor.execute(" CREATE TABLE İF NOT EXİST  öğrenciler (ad TEXT , )")
+
+def
+
+  tablooluştur()
