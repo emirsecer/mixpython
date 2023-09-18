98dd592568cf3eebd30120c9c27d29a70e13f96f
emirsecer <135528914+emirsecer@users.noreply.github.com>
2023-09-18 21:28:32 +0300

    Python öğrenirken yaptığım bir çalışma

---
+++ lib_math.py
@@ -0,0 +1,23 @@
+import math
+
+a= math.log(25,5)
+b=math.log(27,3)
+c=math.sqrt(64)
+d=math.sqrt(144)
+e=math.tan(45)
+
+
+import datetime
+dir(datetime)
+
+
+
+
+
+
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
